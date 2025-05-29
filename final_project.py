d6247f56e4ac52f1624f1dfe65fa7ea6ebf66fbe
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-05-29 11:09:03 -0400

    formatting

--- final_project.py
+++ final_project.py
@@ -21,7 +21,7 @@ fluid_y = -2
 fluid_z = 0
 
 # properties of object
-m = .05  # mass
+m = 0.05  # mass
 cd = 0.47  # drag coefficient
 ball_radius = 0.3  # radius of the ball
 h = 0  # height submerged
@@ -30,22 +30,24 @@ V = 0  # for volume displaced
 
 # BUTTONS FOR CHANGING THE ORIGINAL DENSITY
 
-def change_to_rubber_density(evt):
-    ...
 
-def change_to_metal_density(evt):
-    ...
+def change_to_rubber_density(evt): ...
 
-def change_to_styrofoam_density(evt):
-    ...
+
+def change_to_metal_density(evt): ...
+
+
+def change_to_styrofoam_density(evt): ...
+
 
 canvas(width=600, height=800, resizable=True, visible=True, align="left")
 
 
 rubber_ball_button = button(bind=change_to_rubber_density, text="Rubber Density")
 metal_ball_button = button(bind=change_to_metal_density, text="Metal Density")
-
-
+styrofoam_ball_button = button(
+    bind=change_to_styrofoam_density, text="Styrofoam Density"
+)``
 
 
 y_init = 3
@@ -58,10 +60,14 @@ g2 = graph(width=350, height=250, xtitle=("Time"), ytitle=("Velocity"), align="l
 vyDots = gdots(color=color.red, graph=g2)
 
 
-
 ball = sphere(pos=vector(0, y_init, 0), radius=ball_radius, color=color.red)
 
-box(pos=vector(fluid_x, fluid_y, fluid_z), size=vector(fluid_length, fluid_height, fluid_width), color=vector(0, 0, 1), opacity=0.5)
+box(
+    pos=vector(fluid_x, fluid_y, fluid_z),
+    size=vector(fluid_length, fluid_height, fluid_width),
+    color=vector(0, 0, 1),
+    opacity=0.5,
+)
 
 
 scene.camera.pos = vector(
@@ -72,12 +78,16 @@ scene.camera.pos = vector(
 total_volume = (4 / 3) * pi * pow(ball_radius, 3)
 
 # while yy > -3.65:
-while yy > fluid_y - fluid_height / 2 + ball_radius :
+while yy > fluid_y - fluid_height / 2 + ball_radius:
     rate(1 / dt)
-    gravity_force = - m * G
-    buoyant_force = (P * G * total_volume) if yy <= (fluid_y + fluid_height / 2 + ball_radius) else 0
+    gravity_force = -m * G
+    buoyant_force = (
+        (P * G * total_volume)
+        if yy <= (fluid_y + fluid_height / 2 + ball_radius)
+        else 0
+    )
 
-    fy = gravity_force + buoyant_force # calculating the force of gravity
+    fy = gravity_force + buoyant_force  # calculating the force of gravity
     ay = fy / m  # calculating the acceleration of gravity
     vy = vy + ay * dt  # calculating the gradient of velocity
     yy = yy + vy * dt  # calculating the change in position
